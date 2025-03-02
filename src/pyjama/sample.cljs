089246f3e91b0659ab07e1b5cd7b34a61b511243
Nicolas Modrzyk <hellonico@gmail.com>
2025-03-02 13:37:54 +0900

    - pyjama cljs with auto mind maps

--- src/pyjama/sample.cljs
+++ src/pyjama/sample.cljs
@@ -8,51 +8,79 @@
 
 (defonce state (r/atom {
                         :url      "http://localhost:8888"
-                        :model    "llama3.2"
+                        :model    "tinyllama"
                         :prompt   ""
+                        :thinking false
+                        :mindmap  false
                         :response ""}))
 
 (defn fetch-response []
   (go
+    (swap! state assoc
+           :thinking true)
     (let [res (pyjama.core/ollama
                 (:url @state)
                 :generate
-                {:prompt (:prompt @state) :model (:model @state)}
+                {:prompt (if (:mindmap @state)
+                           (str (:prompt @state) "\nOutput response in markdown.")
+                           (:prompt @state))
+                 :model  (:model @state)}
                 :response)]
-      (swap! state assoc :response (str (<! res))))))
+      (swap! state assoc
+             :thinking false
+             :response (str (<! res))))))
+
+(defn mindmap-view [markdown-text]
+  (when (exists? (.-autoLoader js/markmap))
+    (.renderAll (.-autoLoader js/markmap)))
+  [:div {:class "markmap" :dangerouslySetInnerHTML {:__html markdown-text}}])
 
 
 (defn markdown-view [markdown-text]
-  [:div {:dangerouslySetInnerHTML {:__html (js/marked.parse markdown-text)}}])
+  (if (:mindmap @state)
+    (mindmap-view markdown-text)
+    [:div {:dangerouslySetInnerHTML {:__html (js/marked.parse markdown-text)}}]
+    ))
 
 (defn app []
-  [:div
+  [:div {:on-click (fn[](swap! state assoc :response ""))}
    [:h1 "Pyjama AI - cljs"]
 
-   [:label "Model "] [:br]
-   [:input {:type      "text"
-            :value     (:model @state)
-            :on-change #(swap! state assoc :model (-> % .-target .-value))}]
+   (when (empty? (:response @state))
+     [:div
 
-   [:br] [:br]
+      [:label "Model "] [:br]
+      [:input {:type      "text"
+               :value     (:model @state)
+               :on-change #(swap! state assoc :model (-> % .-target .-value))}]
 
-   [:label "Prompt "] [:br]
-   [:textarea {:rows      3
-               :cols      50
-               :value     (:prompt @state)
-               :on-change #(swap! state assoc :prompt (-> % .-target .-value))}]
+      [:br] [:br]
 
-   [:br] [:br]
+      [:label "Prompt "] [:br]
+      [:textarea {:rows      3
+                  :cols      50
+                  :value     (:prompt @state)
+                  :on-change #(swap! state assoc :prompt (-> % .-target .-value))}]
 
-   [:button {:on-click (fn []
-                         (swap! state assoc :response "...")
-                         (fetch-response))} "Go"]
+      [:br] [:br]
 
-   [:h2 "Response"]
-    [markdown-view (:response @state)]])
+      [:button {:class (if (and (:thinking @state) (not (:mindmap @state))) "loop" "") :on-click (fn []
+                                               (swap! state assoc
+                                                      :mindmap false
+                                                      :response "...")
+                                               (fetch-response))} "Go"]
+
+      [:button {:class (if (and (:thinking @state) (:mindmap @state)) "loop" "") :on-click (fn []
+                            (swap! state assoc
+                                   :mindmap true
+                                   :response "...")
+                            (fetch-response))} "MindMap"]])
+
+   (when (not (empty? (:response @state)))
+     [:div
+       [:h2 (:prompt @state)]
+       [markdown-view (:response @state)]])])
 
 (defn ^:export init []
-
   (js/console.warn "will execute on every code change")
-
   (rd/render [app] (.getElementById js/document "app")))
